b47ab59ebfef72500ca3483cb912c8f0d92a9f79
Claudia Atkinson Bedia <claatkin@c2r10s4.42madrid.com>
2023-04-11 17:24:04 +0200

    Más cambios al atoi y al strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: claatkin <claatkin@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 18:09:04 by claatkin          #+#    #+#             */
-/*   Updated: 2023/04/11 15:15:06 by claatkin         ###   ########.fr       */
+/*   Updated: 2023/04/11 17:11:28 by claatkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,19 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	if (dstsize > 0)
+	while ((dstsize > 0) && (i <= dstsize - 1))
 	{
-		while (i <= dstsize - 1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
+		dst[i] = src[i];
+		i++;
 	}
-	return (dst[i]);
+	return (i);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	char dst [] = "";
 	char src [] = "hola";
 	size_t len = 2;
 	
     printf("%zu\n", ft_strlcpy(dst, src, len));
-}*/
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: claatkin <claatkin@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 19:10:23 by claatkin          #+#    #+#             */
-/*   Updated: 2023/04/11 15:28:27 by claatkin         ###   ########.fr       */
+/*   Updated: 2023/04/11 17:17:46 by claatkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,22 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+//void	*ft_memset(void *b, int c, size_t len);
+//void	ft_bzero(void *s, size_t n);
+//void	*ft_memcpy(void *dst, const void *src, size_t n);
+//void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src,
 			size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src,
-			size_t dstsize);
+//size_t	ft_strlcat(char *dst, const char *src,
+//			size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
+//char	*ft_strchr(const char *s, int c);
+//char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+//void	*ft_memchr(const void *s, int c, size_t n);
+//void	ft_memcmp(const void *s1, const void *s2, size_t n);
+//char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 
 #endif
