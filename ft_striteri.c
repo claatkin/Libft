9363445c62a490dd760a989015d9b4b4067f8fb9
claatkin <claatkin@student.42madrid.com>
2023-06-03 18:00:44 +0200

    Modificada una mini cosa

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: claatkin <claatkin@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 09:50:33 by claatkin          #+#    #+#             */
-/*   Updated: 2023/06/01 16:11:16 by claatkin         ###   ########.fr       */
+/*   Updated: 2023/06/03 17:50:59 by claatkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 
 	i = 0;
-	if (s != '\0' && f != '\0')
+	if (s != NULL && f != NULL)
 	{
 		while (s[i] != '\0')
 		{
